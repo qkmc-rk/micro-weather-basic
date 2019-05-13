587edae86c71a95142e83fd5a1ade867de7393bf
father'slittleRuanZhonghua <1537854187@qq.com>
2019-05-13 20:00:04 +0800

    first commit

---
+++ src/main/java/xyz/ruankun/microweatherbasic/MicroWeatherBasicApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruankun.microweatherbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroWeatherBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroWeatherBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/ruankun/microweatherbasic/MicroWeatherBasicApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.ruankun.microweatherbasic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroWeatherBasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
