2ed8eafe2bcb135fdef973b68a717953c7318285
father'slittleRuanZhonghua <1537854187@qq.com>
2019-05-16 16:33:01 +0800

    add cache implentated by redis

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # micro-weather-basic
 weather service
 > ENCODING: GB2312
+
+### 使用redis可以提高访问效率，减少与三方API的交互，避免被咸鱼
--- src/main/java/xyz/ruankun/microweatherbasic/service/impl/WeatherDataServiceImpl.java
+++ src/main/java/xyz/ruankun/microweatherbasic/service/impl/WeatherDataServiceImpl.java
@@ -21,10 +21,12 @@ package xyz.ruankun.microweatherbasic.service.impl;
 //                      Buddha Bless, No Bug !
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.http.HttpHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -32,21 +34,27 @@ import xyz.ruankun.microweatherbasic.service.WeatherDataService;
 import xyz.ruankun.microweatherbasic.vo.WeatherResponse;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Service
 //@Component
 //@Repository
 public class WeatherDataServiceImpl implements WeatherDataService {
 
+    //to write log
+    private static final Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
     //从springboot的配置文件中读取API
     @Value("${micro-weather-basic.api}")
     private String WEATHER_API;
-
     @Autowired
     private RestTemplate restTemplate;
-
-
+    /**
+     * use to operate the data in/out redis
+     */
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Value("${micro-weather-basic.redis.timeout}")
+    private Long TIME_OUT;
 
     @Override
     public WeatherResponse getDataByCityId(String cityId) {
@@ -60,26 +68,39 @@ public class WeatherDataServiceImpl implements WeatherDataService {
         return this.doGetWeatherData(uri);
     }
 
-
     /**
      * e
      * @param uri
      * @return
      */
     private WeatherResponse doGetWeatherData(String uri){
+        //get the operator provided by stringRedisTemplate to operate the K-V data
+        ValueOperations ops = this.stringRedisTemplate.opsForValue();
+        //uri as the key in redis
+        String key = uri;
         String strBody = null;
-        WeatherResponse weatherResponse = null;
-        ObjectMapper mapper = new ObjectMapper();
-
-        System.out.println(new Date() + "  INFO  ---  " + uri);
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri,String.class);
-        //via status code to judge false or true
-        if(responseEntity.getStatusCodeValue() == 200){
-            //request success,to get data
-            strBody = responseEntity.getBody();
+        //search data from redis first.
+        if(!this.stringRedisTemplate.hasKey(uri)){
+            //sorry but there is no cache
+            logger.info("sorry there is no weather info cache in redis");
+            logger.info("start requesting server at:" + uri);
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri,String.class);
+            //via status code to judge false or true
+            if(responseEntity.getStatusCodeValue() == 200){
+                //request success,to get data
+                strBody = responseEntity.getBody();
+            }
+            //since we have gotten the data.We cache it.
+            ops.set(uri,strBody,TIME_OUT, TimeUnit.SECONDS);
+        }else{
+            //find cache in redis
+            logger.info("find cache in redis server" );
+            logger.info("will not request the api for weather data,request uri:" + uri);
+            strBody = (String) ops.get(uri);
         }
-        System.out.println(new Date() + "  INFO  ---  ");
         //anti serialize the String data to Object
+        ObjectMapper mapper = new ObjectMapper();
+        WeatherResponse weatherResponse = null;
         try {
             weatherResponse = mapper.readValue(strBody, WeatherResponse.class);
         } catch (IOException e) {
